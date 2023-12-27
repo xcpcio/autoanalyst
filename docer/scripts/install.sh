5ca6f31972d506e24df25d6711284f948221ba11
Dup4 <lyuzhi.pan@gmail.com>
2023-12-27 09:04:06 +0800

    chore: docker
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

---
+++ docer/scripts/install.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+sudo apt-get install \
+        git gitk gitweb apache2 apache2-utils phpmyadmin \
+        php5-cli ntp rsync make curl python-yaml \
+        openjdk-7-jdk openjdk-7-jre python-httplib2 python-mysqldb
